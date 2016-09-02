8e6873f7917b590f15518c0466b937dc8d4cbc48
Ryan Vandelinder <ryan@autostructure.io>
2016-09-02 14:31:18 -0400

    test calling install and variables
    Former-commit-id: aa1cbaf732520f2676d0ad7fd70d1c763c3ea473

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,6 +3,8 @@
 # This class is called from tripwire for install.
 #
 class tripwire::install {
-
+  notify { 'This is the install class ':
+      message => "This is the install class --server-host $twserverhost --server-port $twport --passphrase $twpassphrase --enable-fips",
+    }
 
 }
