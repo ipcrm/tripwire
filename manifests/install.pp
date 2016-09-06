7a4e66972bd71aed39d1582633c1d2db9c99efcd
Ryan Vandelinder <ryan@autostructure.io>
2016-09-06 10:50:47 -0400

    starting install tripwire
    Former-commit-id: 5d28877014751d21b1e633f6f62f2fff7eaae482

--- manifests/install.pp
+++ manifests/install.pp
@@ -7,4 +7,25 @@ class tripwire::install {
       message => "This is the install class --server-host $twserverhost --server-port $twport --passphrase $twpassphrase --enable-fips",
     }
 
+  #  file {'/tmp/':
+  #      ensure => 'directory',
+  #      owner  => 'root',
+  #      group  => 'root',
+  #    }
+
+  #    file{'/tmp/te_agent_8.4.2_en_linux_x86_64.tgz':
+  #      ensure => file,
+  #      mode   => '0644',
+  #      source => 'puppet:///modules/tripwire/te_agent_8.4.2_en_linux_x86_64.tgz',
+  #      notify => Exec['unpack tripwire'],
+  #    }
+
+  #    exec { 'unpack tripwire':
+  #      cwd         => '/tmp',
+        #path        => '/tmp/tripwire',
+  #      command     => '/bin/tar xvf te_agent_8.4.2_en_linux_x86_64.tgz',
+  #      refreshonly => true,
+  #    }
+
+
 }
