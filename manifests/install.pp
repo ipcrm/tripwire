2817bb74ed1f391ec85fdd166a2b8d57ad3ecf07
RYAN VANDELINDER <ryan@autostructure.io>
2016-09-07 09:23:26 -0400

    add comma
    Former-commit-id: 42c729062b54f5bca5160c347bc2f8c087f1b19f

--- manifests/install.pp
+++ manifests/install.pp
@@ -20,8 +20,8 @@ class tripwire::install
   exec { 'installtripagt':
   cwd     => $::tripwire::tripwire_installdir,
   path    => $::tripwire::tripwire_installdir,
-  command => 'pwd'#"./te_agent.bin --eula accept --silent --server-host ${::tripwire::tripip} --server-port ${::tripwire::twtripport} --passphrase ${::tripwire::pass} --enable-fips",
-  require => File['/tmp/te_agent_8.4.2_en_linux_x86_64/te_agent.bin']
+  command => 'pwd',#"./te_agent.bin --eula accept --silent --server-host ${::tripwire::tripip} --server-port ${::tripwire::twtripport} --passphrase ${::tripwire::pass} --enable-fips",
+  require => File['/tmp/te_agent_8.4.2_en_linux_x86_64/te_agent.bin'],
   #refreshonly => true,
 
 
