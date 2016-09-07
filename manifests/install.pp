5d5a97cd4c07eecee106bfcfb220680ccec3fa5d
RYAN VANDELINDER <ryan@autostructure.io>
2016-09-06 22:55:00 -0400

    place exec back on
    Former-commit-id: 4ddab162b02cb50bfb9ebd8d8707a02e05390bf3

--- manifests/install.pp
+++ manifests/install.pp
@@ -18,12 +18,12 @@ class tripwire::install
 }
 
 
-#  exec { 'installtripagt':
-#  cwd         => '/tmp/te_agent_8.4.2_en_linux_x86_64/',
-#  path        => '.'
-#  command     => "./te_agent.bin --eula accept --silent --server-host ${::tripwire::tripip} --server-port ${::tripwire::twtripport} --passphrase ${::tripwire::pass} --enable-fips",
-#  refreshonly => true,
+  exec { 'installtripagt':
+  cwd         => '/tmp/te_agent_8.4.2_en_linux_x86_64/',
+  path        => '/tmp/te_agent_8.4.2_en_linux_x86_64/',
+  command     => "./te_agent.bin --eula accept --silent --server-host ${::tripwire::tripip} --server-port ${::tripwire::twtripport} --passphrase ${::tripwire::pass} --enable-fips",
+  refreshonly => true,
 
 
-#}
+}
 }
