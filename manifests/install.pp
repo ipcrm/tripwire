ce2c51f3ce5b8318c17ec353cecb3dd75a43663c
RYAN VANDELINDER <ryan@autostructure.io>
2016-09-07 11:14:41 -0400

    add exec provider
    Former-commit-id: e7090fc7516dcff0f68e6572fbab3a8afffc6c94

--- manifests/install.pp
+++ manifests/install.pp
@@ -19,6 +19,7 @@ class tripwire::install
   exec { 'installtripagt':
   #cwd     => '/tmp',
   path      => $::tripwire::tripwire_installdir,
+  provider  => 'shell',
   command   => "te_agent.bin --eula accept --silent --server-host ${::tripwire::twip} --server-port ${::tripwire::twtripport} --passphrase ${::tripwire::pass} --enable-fips",
   require   => File['/tmp/te_agent_8.4.2_en_linux_x86_64/te_agent.bin'],
   creates   => '/usr/local/tripwire',
