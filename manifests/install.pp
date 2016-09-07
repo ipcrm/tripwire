52d0578fa745b9b538804eac878d999dd0567711
RYAN VANDELINDER <ryan@autostructure.io>
2016-09-07 07:02:03 -0400

    paramertize dir
    Former-commit-id: 006242984f52336df0f637739acbb2ba997e5510

--- manifests/install.pp
+++ manifests/install.pp
@@ -19,8 +19,8 @@ class tripwire::install
 
 
   exec { 'installtripagt':
-  cwd         => '/tmp/te_agent_8.4.2_en_linux_x86_64/',
-  path        => '/tmp/te_agent_8.4.2_en_linux_x86_64/',
+  cwd         => $::tripwire::tripwire_installdir,
+  path        => $::tripwire::tripwire_installdir,
   command     => "./te_agent.bin --eula accept --silent --server-host ${::tripwire::tripip} --server-port ${::tripwire::twtripport} --passphrase ${::tripwire::pass} --enable-fips",
   refreshonly => true,
 
