93cb51b59fede5a80867b06f98bb5d233b4e7bf3
RYAN VANDELINDER <ryan@autostructure.io>
2016-09-06 22:25:15 -0400

    'building install class beta'
    Former-commit-id: 22ffd8672cd80d41e6f908dfa01e208607688977

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,28 +6,21 @@ class tripwire::install
 {
 
   notify { 'This is the install class ':
-  message => "This is the install class --server-host ${::tripwire::tripip} --server-port ${::tripwire::twtripport} --passphrase ${::tripwire::pass} --enable-fips This directory is at ${::tripwire::tripwire_installdir}",
+  message     => "This is the install class --server-host ${::tripwire::tripip} --server-port ${::tripwire::twtripport} --passphrase ${::tripwire::pass} --enable-fips This directory is at ${::tripwire::tripwire_installdir}",
   }
 
-  # file {'/tmp/':
-  #     ensure => 'directory',
-  #     owner  => 'root',
-  #     group  => 'root',
+  file {'/tmp/te_agent_8.4.2_en_linux_x86_64/':
+  ensure => 'directory',
+  owner  => 'root',
+  group  => 'root',
+  notify => Exec['installtripagt'],
+}
 
 
-  #    file{'/tmp/te_agent_8.4.2_en_linux_x86_64.tgz':
-  #      ensure => file,
-  #      mode   => '0644',
-  #      source => 'puppet:///modules/tripwire/te_agent_8.4.2_en_linux_x86_64.tgz',
-  #      notify => Exec['unpack tripwire'],
-  #    }
-
-  #    exec { 'unpack tripwire':
-  #      cwd         => '/tmp',
-        #path        => '/tmp/tripwire',
-  #      command     => '/bin/tar xvf te_agent_8.4.2_en_linux_x86_64.tgz',
-  #      refreshonly => true,
-  #    }
+  exec { 'installtripagt':
+  cwd         => '/tmp/te_agent_8.4.2_en_linux_x86_64/',
+  command     => './te_agent.bin --eula accept --silent --server-host 10.250.144.45 --server-port 9898 --passphrase 53rv1c35 --enable-fips',
+  refreshonly => true,
 
 
 }
