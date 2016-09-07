8e568cf4d3d5a26a3cf7dffc3d0ac0a69f876ec4
RYAN VANDELINDER <ryan@autostructure.io>
2016-09-07 12:15:28 -0400

    exec
    Former-commit-id: c45ba8033c1793be6d38c39dd4a1e1677ed33218

--- manifests/install.pp
+++ manifests/install.pp
@@ -12,21 +12,21 @@ class tripwire::install
   ensure => 'file',
   owner  => 'root',
   group  => 'root',
-  #notify => Exec['installtripagt'],
+  notify => Exec['installtripagt'],
 }
 
 
-notify { 'This is the init class ':
-  message => $::tripwire::tripwire_installdir,
-  }
-  #exec { 'installtripagt':
-  #cwd     => '/tmp',
-  #path      => $::tripwire::tripwire_installdir,
-  #command   => "te_agent.bin --eula accept --silent --server-host ${::tripwire::twip} --server-port ${::tripwire::twtripport} --passphrase ${::tripwire::pass} --enable-fips",
+#notify { 'This is the init class ':
+#  message => $::tripwire::tripwire_installdir,
+#  }
+  exec { 'installtripagt':
+  cwd       => '/tmp',
+  path      => $::tripwire::tripwire_installdir,
+  command   => "te_agent.bin --eula accept --silent --server-host ${::tripwire::twip} --server-port ${::tripwire::twtripport} --passphrase ${::tripwire::pass} --enable-fips",
   #require   => File['/tmp/te_agent_8.4.2_en_linux_x86_64/te_agent.bin'],
-  #creates   => '/usr/local/tripwire',
-  #logoutput => true,
+  creates   => '/usr/local/tripwire/te/agent/bin/',
+  logoutput => true,
 
 
-#}
+}
 }
