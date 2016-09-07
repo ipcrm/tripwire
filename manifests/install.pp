2d462c568aef6dcfd20477d1088f1144eebab53e
RYAN VANDELINDER <ryan@autostructure.io>
2016-09-07 09:07:36 -0400

    install cleanup
    Former-commit-id: 4c444dbe9ff674ec7ab2f484ab11ecfdcf0076bd

--- manifests/install.pp
+++ manifests/install.pp
@@ -5,10 +5,8 @@
 class tripwire::install
 {
 
-  notify { 'This is the install class':
-  message     => "./te_agent.bin --eula accept --silent --server-host ${::tripwire::tripip} --server-port ${::tripwire::twtripport} --passphrase ${::tripwire::pass} --enable-fips",
   #This directory is at ${::tripwire::tripwire_installdir}",
-  }
+
 
   file {'/tmp/te_agent_8.4.2_en_linux_x86_64/te_agent.bin':
   ensure => 'file',
