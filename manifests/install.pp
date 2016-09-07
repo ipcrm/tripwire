e4fb7836c9675f0f7f0cd6d6fad3794685c09f44
RYAN VANDELINDER <ryan@autostructure.io>
2016-09-07 13:05:36 -0400

    took off notify until test done
    Former-commit-id: 2266ce183c96261d4baa5c24e4ab2ff2a98edcaf

--- manifests/install.pp
+++ manifests/install.pp
@@ -12,7 +12,7 @@ class tripwire::install
   ensure => 'file',
   owner  => 'root',
   group  => 'root',
-  notify => Exec['installtripagt'],
+  #notify => Exec['installtripagt'],
 }
 
 
@@ -21,7 +21,7 @@ class tripwire::install
 #  }
   exec { 'installtripagt':
   cwd       => '/tmp',
-  path      => $::tripwire::tripwire_installdir/,
+  path      => $::tripwire::tripwire_installdir,
   command   => "te_agent.bin --eula accept --silent --server-host ${::tripwire::twip} --server-port ${::tripwire::twtripport} --passphrase ${::tripwire::pass} --enable-fips",
   #require   => File['/tmp/te_agent_8.4.2_en_linux_x86_64/te_agent.bin'],
   creates   => '/usr/local/tripwire/te/agent/bin/',
