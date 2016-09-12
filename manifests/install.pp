602dc36b47e830205840738735a70b2a06ff7655
Bryan Belanger <bbelanger@azcender.com>
2016-09-12 18:59:22 -0400

    Exploratory surgery
    Former-commit-id: 18dfd41b0e9c3411ae5d551260c66b610c0f3991

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,29 +2,25 @@
 #
 # This class is called from tripwire for install.
 #
-class tripwire::install
-{
+class tripwire::install {
 
   #This directory is at ${::tripwire::tripwire_installdir}",
 
 
   file {'/tmp/te_agent_8.4.2_en_linux_x86_64/te_agent.bin':
-  ensure => 'file',
-  owner  => 'root',
-  group  => 'root',
-  notify => Exec['installtripagt'],
-}
+    ensure => 'file',
+    owner  => 'root',
+    group  => 'root',
+    notify => Exec['installtripagt'],
+  }
 
   exec { 'installtripagt':
-  cwd       => '/tmp',
-  path      => [$::tripwire::tripwire_installdir,'/bin','/usr/bin'],
-  command   => "te_agent.bin --eula accept --silent --server-host ${::tripwire::twip} --server-port ${::tripwire::twtripport} --passphrase ${::tripwire::pass} --enable-fips",
-  require   => File['/tmp/te_agent_8.4.2_en_linux_x86_64/te_agent.bin'],
-  creates   => '/usr/local/tripwire/te/agent/bin/',
-  logoutput => true,
-  timeout   => 1800,
-
-
-
-}
+    cwd       => '/tmp',
+    path      => [$::tripwire::tripwire_installdir,'/bin','/usr/bin'],
+    command   => "te_agent.bin --eula accept --silent --server-host ${::tripwire::twip} --server-port ${::tripwire::twtripport} --passphrase ${::tripwire::pass} --enable-fips",
+    require   => File['/tmp/te_agent_8.4.2_en_linux_x86_64/te_agent.bin'],
+    creates   => '/usr/local/tripwire/te/agent/bin/',
+    logoutput => true,
+    timeout   => 1800,
+  }
 }
