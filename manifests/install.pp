96b20309e5be5e43d14679eb46a7d69841c131e0
RYAN VANDELINDER <ryan@autostructure.io>
2016-10-24 21:54:04 -0400

    add / to line 9

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,7 +6,7 @@ class tripwire::install {
 
   #This directory is at ${::tripwire::tripwire_installdir}",
 
-  file {'te_agent_8.4.2_en_linux_x86_64/te_agent.bin':
+  file {'/te_agent_8.4.2_en_linux_x86_64/te_agent.bin':
   ensure => 'file',
   owner  => 'root',
   group  => 'root',
