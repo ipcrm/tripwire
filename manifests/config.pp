c2010ed2c0483cbcb9f921e542e22c7185e4029e
Ryan Vandelinder <ryan@autostructure.io>
2016-11-18 15:33:24 -0500

    make files to ensure before service start

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,4 +3,20 @@
 # This class is called from tripwire for service config.
 #
 class tripwire::config {
+
+  file { '/etc/init.d/twdaemon':
+    ensure  => 'file',
+    mode    => '0755',
+    owner   => 'root',
+    group   => 'root',
+    source  => 'puppet:///modules/tripwire/twdaemon',
+}
+
+file { '/etc/rc.d/init.d/twdaemon':
+  ensure  => 'file',
+  mode    => '0755',
+  owner   => 'root',
+  group   => 'root',
+  source  => 'puppet:///modules/tripwire/twdaemon',
+}
 }
