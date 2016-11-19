2a3cf8d5bc6c3382117e8e2758ac833811932438
RYAN VANDELINDER <ryan@autostructure.io>
2016-11-19 14:42:30 -0500

    Correct puppet lint error

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,18 +5,18 @@
 class tripwire::config {
 
   file { '/etc/init.d/twdaemon':
-    ensure  => 'file',
-    mode    => '0755',
-    owner   => 'root',
-    group   => 'root',
-    source  => 'puppet:///modules/tripwire/twdaemon',
+    ensure => 'file',
+    mode   => '0755',
+    owner  => 'root',
+    group  => 'root',
+    source => 'puppet:///modules/tripwire/twdaemon',
 }
 
 file { '/etc/rc.d/init.d/twdaemon':
-  ensure  => 'file',
-  mode    => '0755',
-  owner   => 'root',
-  group   => 'root',
-  source  => 'puppet:///modules/tripwire/twdaemon',
+  ensure => 'file',
+  mode   => '0755',
+  owner  => 'root',
+  group  => 'root',
+  source => 'puppet:///modules/tripwire/twdaemon',
 }
 }
