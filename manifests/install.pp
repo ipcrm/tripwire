335d191d06a609b03617abf472b27a9829788dea
RYAN VANDELINDER <ryan@autostructure.io>
2016-11-20 18:04:47 -0500

    correcting tripwire dir

--- manifests/install.pp
+++ manifests/install.pp
@@ -7,7 +7,7 @@ class tripwire::install {
   #This directory is at ${::tripwire::tripwire_installdir}",
 
 
-  file {'/usr/local/te_agent_8.4.2_en_linux_x86_64/te_agent.bin':
+  file "${tripwire_installdir}/te_agent_8.4.2_en_linux_x86_64/te_agent.bin":
     ensure => 'file',
     owner  => 'root',
     group  => 'root',
@@ -15,10 +15,10 @@ class tripwire::install {
   }
 
   exec { 'installtripagt':
-    cwd       => '/tmp',
+    cwd       => $tripwire_installdir,
     path      => [$::tripwire::tripwire_installdir,'/bin','/usr/bin'],
     command   => "te_agent.bin --eula accept --silent --server-host ${::tripwire::twip} --server-port ${::tripwire::twtripport} --passphrase ${::tripwire::pass} --enable-fips",
-    creates   => '/usr/local/tripwire/te/agent/bin/',
+    creates   => "${tripwire_installdir}/tripwire/te/agent/bin/",
     logoutput => true,
     timeout   => 1800,
   }
