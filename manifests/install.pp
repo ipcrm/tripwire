83b996277b0660adb570bac44187cde3a0f82ddf
RYAN VANDELINDER <ryan@autostructure.io>
2016-11-20 19:55:00 -0500

    dir

--- manifests/install.pp
+++ manifests/install.pp
@@ -15,7 +15,7 @@ class tripwire::install {
 
   exec { 'installtripagt':
     cwd       => "${::tripwire::tripwire_installdir}/te_agent_8.4.2_en_linux_x86_64/",
-    path      => ['$twdir','/bin','/usr/bin'],
+    path      => [$::tripwire::tripwire_installdir,'/bin','/usr/bin'],
     command   => "te_agent.bin --eula accept --silent --server-host ${::tripwire::twip} --server-port ${::tripwire::twtripport} --passphrase ${::tripwire::pass} --enable-fips",
     creates   => "${::tripwire::tripwire_installdir}/tripwire/te/agent/bin/",
     logoutput => true,
