ba996cfb657f48968958f02f75515f5f4481744a
RYAN VANDELINDER <ryan@autostructure.io>
2016-11-20 19:42:32 -0500

    try hiera var

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,7 +6,7 @@ class tripwire::install {
 
   #This directory is at ${::tripwire::tripwire_installdir}",
 
-  file { "'${::tripwire::tripwire_installdir}/te_agent_8.4.2_en_linux_x86_64/te_agent.bin'" :
+  file { "${::twdir}/te_agent_8.4.2_en_linux_x86_64/te_agent.bin" :
     ensure => 'file',
     owner  => 'root',
     group  => 'root',
@@ -14,10 +14,10 @@ class tripwire::install {
   }
 
   exec { 'installtripagt':
-    cwd       => "${::tripwire::tripwire_installdir}/te_agent_8.4.2_en_linux_x86_64/",
-    path      => ['$::tripwire::tripwire_installdir','/bin','/usr/bin'],
+    cwd       => "${::twdir}/te_agent_8.4.2_en_linux_x86_64/",
+    path      => ['$twdir','/bin','/usr/bin'],
     command   => "te_agent.bin --eula accept --silent --server-host ${::tripwire::twip} --server-port ${::tripwire::twtripport} --passphrase ${::tripwire::pass} --enable-fips",
-    creates   => "${::tripwire::tripwire_installdir}/tripwire/te/agent/bin/",
+    creates   => "${::twdir}/tripwire/te/agent/bin/",
     logoutput => true,
     timeout   => 1800,
   }
