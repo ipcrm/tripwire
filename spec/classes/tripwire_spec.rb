f832949180f862eeac89ea6cf1095f6ea9508036
RYAN VANDELINDER <ryan@autostructure.io>
2016-11-21 13:16:03 -0500

    add commas close tripwire DO

--- spec/classes/tripwire_spec.rb
+++ spec/classes/tripwire_spec.rb
@@ -4,11 +4,11 @@ describe 'tripwire' do
   describe 'tripwire', :type => :class do
   let(:params) do
     {
-      master_host: '10.250.144.45'
-      master_port: 9898
-      master_passcode: 'fs pass'
-      client_installdir: '/usr/local'
-
+      master_host: '10.250.144.45',
+      master_port: 9898,
+      master_passcode: 'fs pass',
+      client_installdir: '/usr/local',
+}
   context 'supported operating systems' do
     on_supported_os.each do |os, facts|
       context "on #{os}" do
