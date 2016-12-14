444094e3e4f7d4b2f912a57002ee1ac4566ac6b0
Ryan Vandelinder <ryan@autostructure.io>
2016-12-14 14:08:03 -0500

    Is this windows ?

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,36 +3,7 @@
 # This class is called from tripwire for service config.
 #
 class tripwire::config {
-  if $::operatingsystemrelease =~ /^6.*/ {
 
-  file { '/etc/init.d/twdaemon':
-    ensure  => 'file',
-    mode    => '0755',
-    owner   => 'root',
-    group   => 'root',
-    content => epp(
-      'tripwire/twdaemon.epp',
-      {
-        client_installdir => $::tripwire::client_installdir
-      }
-    ),
-  }
-
-  file { '/etc/rc.d/init.d/twdaemon':
-    ensure  => 'file',
-    mode    => '0755',
-    owner   => 'root',
-    group   => 'root',
-    content => epp(
-      'tripwire/twdaemon.epp',
-      {
-        client_installdir => $::tripwire::client_installdir
-      }
-    ),
-  }
-
-
-elseif $::operatingsystemrelease =~ /^7.*/ {
 
   file { '/etc/systemd/system/twdaemon':
     ensure  => 'file',
@@ -46,6 +17,7 @@ elseif $::operatingsystemrelease =~ /^7.*/ {
       }
     ),
   }
+}
 
 #  file { '/etc/rc.d/init.d/twdaemon':
 #    ensure  => 'file',
@@ -59,7 +31,3 @@ elseif $::operatingsystemrelease =~ /^7.*/ {
 #      }
 #    ),
 #  }
-}
-
-}
-}
