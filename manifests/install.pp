81cab7b20eebc0df61b883438273c6762f5ef77c
ipcrm <mcadorette@gmail.com>
2016-12-20 11:04:23 -0500

    Updating install dir

--- manifests/install.pp
+++ manifests/install.pp
@@ -7,7 +7,7 @@ class tripwire::install {
   exec { 'installtripagt':
     cwd       => $::tripwire::install_media_dir,
     path      => [$::tripwire::install_media_dir,'/bin','/usr/bin'],
-    command   => "te_agent.bin --eula accept --silent --server-host ${::tripwire::master_host} --server-port ${::tripwire::master_port} --passphrase ${::tripwire::master_passcode} --enable-fips",
+    command   => "te_agent.bin --eula accept --silent --server-host ${::tripwire::master_host} --server-port ${::tripwire::master_port} --passphrase ${::tripwire::master_passcode} --install-dir ${::tripwire::client_installdir}",
     creates   => "${::tripwire::client_installdir}/tripwire/te/agent/bin/",
     logoutput => true,
     timeout   => 1800,
